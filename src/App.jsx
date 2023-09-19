c980d199080912ffc0d17e0f356a7d1108c99aa2
Julietadallapozza <dallapozzajulieta@gmail.com>
2023-09-19 12:52:01 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import "./App.css";
+import Contacts from './contacts.json';
+import { useState } from "react";
+
 
 function App() {
+  // and set the array from contacts-data.json as the initial state
+  const [contacts, setContacts] = useState(Contacts);
+
   return (
-    <div className="App">
+    <div className="fex flex wrapped">
+
       <h1>LAB | React IronContacts</h1>
+      <table>
+        {contacts.map(name => {
+          console.log(contacts)
+          return <p> {name} </p>
+        })}
+      </table>
+
+      <Contacts />
     </div>
+
   );
 }
 
