1310d16dcf565d5a0a0abbacaca64b8c35969049
Julietadallapozza <dallapozzajulieta@gmail.com>
2023-09-19 13:06:27 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import "./App.css";
-import Contacts from './contacts.json';
+import contactsData from './contacts.json';
 import { useState } from "react";
 
 
 function App() {
   // and set the array from contacts-data.json as the initial state
-  const [contacts, setContacts] = useState(Contacts);
+  const [contacts, setContacts] = useState(contactsData);
 
   return (
     <div className="fex flex wrapped">
 
       <h1>LAB | React IronContacts</h1>
       <table>
-        {contacts.map(name => {
-          console.log(contacts)
-          return <p> {name} </p>
+        {contacts.map((contact) => {
+          return <td key={contact.name}>{contact.name}</td>
         })}
       </table>
 
-      <Contacts />
     </div>
 
   );
