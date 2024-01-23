a0cb20140d08e4044407be3ecea7e9fd8fd412a6
Pabloluis9 <140593315+Pabloluis9@users.noreply.github.com>
2024-01-23 20:55:38 +0100

    done

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,76 @@
+import actorsData from "../contacts.json";
+import { useState } from "react";
+import Button from './Button'
+import './Contacts.css'
+
+
+const Actors = () => {
+    const [allActors, setAllActors] = useState(actorsData.slice(5))
+    const [actors, setActors] = useState(actorsData.slice(0, 5))
+
+
+    const onAddRandomActor = () => {
+        if (allActors.lenght === 0) {
+            return alert('No hay más actores que añadir!!')
+        }
+    
+    const randomIndex = Math.floor(Math.random() * allActors.length)
+    setAllActors(allActors.filter((actors, index) => index !== randomIndex));
+    
+    const itemToAdd = allActors[randomIndex];
+    setActors([itemToAdd, ...actors])
+    
+    }
+
+    const onDelete = (id) => {
+        const newArr = actors.filter((actor) => {
+            return actor.id !== id
+        })
+    
+        setActors(newArr);
+
+    }
+
+
+    return (
+        <div>
+          <Button type="success" onClick={onAddRandomActor} disabled={actors.length === 0}>
+            Add new actor
+          </Button>
+    
+          <table className="table table-bordered mt-3">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">Picture</th>
+                <th scope="col">Name</th>
+                <th scope="col">Popularity</th>
+                <th scope="col">Won Oscar</th>
+                <th scope="col">Won Emy</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {actors.map((actor) => (
+                <tr key={actor.id}>
+                  <td>
+                    <img src={actor.pictureUrl} className="card-img-top" alt={actor.name} />
+                  </td>
+                  <td>{actor.name}</td>
+                  <td>{actor.popularity}</td>
+                  <td>{actor.wonOscar ? '🏆' : ''}</td>
+                  <td>{actor.wonEmy ? '🏆' : ''}</td>
+                  <td>
+                    <Button className="btn btn-danger" type="danger" onClick={() => onDelete(actor.id)}>
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    };
+    
+    export default Actors;
+    
