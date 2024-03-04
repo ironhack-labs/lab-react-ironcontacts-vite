714e87776565245ec9a41a0dbcd7e2150035ccdf
Idir FOUQUET <fouquetidir@gmail.com>
2024-03-04 21:44:28 +0100

    Iteration 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,22 @@ import { useState } from "react";
 
 //const [shortList, setContacts] = useState()
 
+const wonOscar = (contact) => {
+  if(contact.wonOscar) {
+    return "🏆"
+  } else {
+    return ""
+  }
+}
+
+const wonEmmy = (contact) => {
+  if(contact.wonEmmy) {
+    return "🌟"
+  } else {
+    return ""
+  }
+}
+
 function App() {
   return (
     <div className="App">
@@ -14,14 +30,19 @@ function App() {
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            <th>Won <br />Oscar</th>
+            <th>Won <br />Emmy</th>
           </tr>
         </thead>
         <tbody>
         {contacts.slice(0,5).map(contact => (
-          <tr key={contact.id}> {/* Assurez-vous que chaque contact a un identifiant unique pour la clé */}
+          <tr key={contact.id}> 
               <td><img src={contact.pictureUrl} alt={contact.name} style={{ width: '50px' }}/></td> {/* Utilisation de <td> pour les images */}
               <td>{contact.name}</td>
-              <td>{contact.popularity.toFixed(2)}</td> {/* Formatage de la popularité pour n'afficher que deux décimales */}
+              <td>{contact.popularity.toFixed(2)}</td>
+              <td>{wonOscar(contact)}</td>
+              <td>{wonEmmy(contact)}</td>
+
             </tr>
         )
         )}
