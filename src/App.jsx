82a9e607e9b9631720e76fee38e6b7afeb129d46
Nil E <nil.erdenn@gmail.com>
2024-05-10 21:23:23 +0200

    iteration 1 done only

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import "./App.css";
+import ContactsList from "./contacts.json";
+
+const contacts = ContactsList.slice(0,5);
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+      <table>
+      <thead>
+    <tr>
+      <th>Picture</th>
+      <th>Name</th>
+      <th>Popularity</th>
+    </tr>
+  </thead>
+  <tbody>
+    { contacts.map(celeb => {
+        return (
+          <tr>
+            <td>
+              <img src={celeb.pictureUrl} alt={celeb.name}></img>
+            </td>
+            <td>
+              { celeb.name }
+            </td>
+            <td>
+              { celeb.popularity }
+            </td> 
+          </tr>)
+      })}
+    </tbody>
+      </table>
     </div>
   );
 }
