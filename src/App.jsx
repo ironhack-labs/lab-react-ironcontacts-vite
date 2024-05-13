3dad5cd446c69128cf2e4e78641094cebbc68610
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-05-13 19:02:43 +0100

    Add and Delet buttons

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,33 @@ import { useState } from "react";
 
 function App() {
 
-  const[contacts, setContacts] = useState(contactsData.slice(0, 5));
+  const [contacts, setContacts] = useState(contactsData.slice(0, 5)); /* !!! useState([...contactsData.slice(0, 5)]); */
+  /* const copiedContacts = [...contacts] */
 
-
-  const[restOfContacts, setRestOfContacts] = useState([...contactsData.slice(5)]);
-
+  const [restOfContacts, setRestOfContacts] = useState(contactsData.slice(5));
 
   const randomContact = restOfContacts[Math.floor(Math.random() * restOfContacts.length)];
+
+  /* !!! const copiedContacts = [...contacts] */
+
+  const addContact = (contactId) => {
+    /* !!! setContacts(contacts.push(randomContact)); */
+    setContacts([...contacts, randomContact]); 
+    setRestOfContacts(restOfContacts.filter(contact => contactId !== contact.id))
+  }
+
+  const deleteContact = (contactId) => {
+    const filteredContacts = contacts.filter(contact => contactId !== contact.id)
+    setContacts(filteredContacts);
+  }
   
   
   return (
     <div className="App">
       <h1>IronContacts</h1>
-      {/* why it doesn't work: onClick={() => setContacts((contacts) => contacts.push(randomContact))} */}
-      {/*       <button onClick={() => {
-        setContacts((contacts) => contacts.push(randomContact));
-        setRestOfContacts((restOfContacts) => restOfContacts.filter(!randomContact));
-        }
-      }>Add Random Contact</button> */}
-      {/* {contacts.push(randomContact)} - it pushes a new contact when refreshing the page */}
 
-      <button onClick={() => contacts.push(randomContact)}>Add Random Contact</button>
+      <button onClick={addContact}>Add Random Contact</button>
+
       <button>Sort by name</button>
       <button>Sort by popularity</button>
       <table>
@@ -41,19 +47,19 @@ function App() {
         </thead>
         <tbody>
 
-                {contacts.map((contact) => {
-                  
-                  return (
-                    <tr key={contact.id}>
-                    <td><img className="picture" key = {contact.id} src={contact.pictureUrl}></img></td>
-                    <td><p className="name" key = {contact.id}>{contact.name}</p></td>
-                    <td><p key = {contact.id}>{contact.popularity.toFixed(2)}</p></td>
-                    <td>{ contact.wonOscar ? <p>🏆</p> : ""} </td>
-                    <td>{ contact.wonEmmy ? <p>🌟</p> : ""}</td>
-                    <td><button onClick={() => contacts.deleteHandler(contacts.contact.id)}>Delete</button></td>
-                </tr>
-                  )
-                })}
+          {contacts.map(contact => {
+            
+            return (
+              <tr key={contact.id}>
+                <td><img className="picture" key = {contact.id} src={contact.pictureUrl}></img></td>
+                <td><p className="name" key = {contact.id}>{contact.name}</p></td>
+                <td><p key = {contact.id}>{contact.popularity.toFixed(2)}</p></td>
+                <td>{ contact.wonOscar ? <p>🏆</p> : ""} </td>
+                <td>{ contact.wonEmmy ? <p>🌟</p> : ""}</td>
+                <td><button onClick={() => deleteContact(contact.id)}>Delete</button></td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
 
