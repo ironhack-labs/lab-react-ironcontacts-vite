0efb8b5deb785053aafed069d2415478af93054b
Sven Ole Spindler <s.spindler@gmx.de>
2024-11-10 14:32:44 +0100

    implemented and styled contact table

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import "./App.css";
+import contactsData from "./contacts.json";
+
+const contacts = contactsData.slice(0, 5);
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React IronContacts</h1>
+      <h1>IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => (
+            <tr key={contact.id}>
+              <td>
+                <img src={contact.pictureUrl} alt={contact.name} />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity.toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
